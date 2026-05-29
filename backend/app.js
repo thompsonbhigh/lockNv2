364a9f27654108b78191835b96e7d1ad83660c08
thompsonbhigh <thompsonbhigh@gmail.com>
2026-05-28 19:56:39 -0500

    https only

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'lax',
         maxAge: 1000 * 60 * 60 * 24 * 7,
     },
