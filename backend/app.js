e059373c60fe189d810462fc0078ae1083698ffb
thompsonbhigh <thompsonbhigh@gmail.com>
2026-05-28 19:22:11 -0500

    Fixing fetches

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'lockn.up.railway.app'],
     credentials: true,
 }));
 
